7110f75c7aa57473c4cc215a8be651aa870033fc
m0rv1nd <168345833+m0rv1nd@users.noreply.github.com>
2024-11-07 20:05:15 +0100

    feat: add choice of number of rounds and description in README.md

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # rock-paper-scissors
+
+This simple game of rock & paper & scissors was written as part of a project assignment for The Odin Project course.
+
+HOW TO PLAY?
+
+After downloading the files and running index.html in your web browser, a window will pop up asking you to type “rock”, “paper” or “scissors”. You have to type in EXACTLY one of these words, although they do not necessarily have to be in lowercase you can also type in “Rock” or “PaPeR”. The program is very intelligent and will convert the user's entered word to the target value in lowercase :D When my programming skills improve I will try to add an appropriate message when the user: 
+
+a) will not type anything and press “ok”
+b) press “cancel”
+c) type an incorrect word, e.g. make a spelling mistake 
+
+You can also select the number of rounds by editing the script.js file and specifying the value in the playGame function call at line 157.
+
+!Attention!
+
+I noticed that the prompt method causes that the first time the program is loaded, the user's and computer's choices are not displayed on the fly, and it does not display who won the round. You have to play the game normally by typing three consecutive values, after which everything will be printed to the console at once. After the next browser refresh everything should work as expected. This is caused by the prompt method. 
+
+Enjoy! :)
+ 
+FUTURE DEVELOPMENT 
+
+- Display a message in case the user:
+
+    a) does not enter anything and presses “ok”
+    b) press “cancel”
+    c) enters an incorrect word, e.g. makes a typo
+
+- Add GUI 
+
+- Solve the prompt method bug
--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 /*
-   PLAN
+!ADDITIONAL FUNCTIONALITIES!
+
+*  Added the ability to choose the number of rounds
+    
+
+PLAN
  
 1. Create a function called getComputerChoice, which returns a string rock or paper or scissors.
    Use Math.floor(Math.random() * 3); for this purpose.
@@ -11,7 +16,7 @@
    Create a function playRound, which takes as parameter the user's choice and the computer's choice. 
    Then a single round is played, the variable corresponding to the winner of the round is incremented and a message is displayed (using console.log) who won the round. Consider using the conditional statement if... else for a total of 9 cases (for each user choice 3 possible choices of the computer).    
 5. Whole game.
-   Create a playGame function to play a full game consisting of 5 rounds. Move the playRound function to playGame. 
+   Create a playGame function to play a full game consisting of 5 rounds. Move the playRound function and scores variables to playGame. 
    Play 5 rounds, after which the winner should be displayed using console.log. You can call the playRound function 5 times using the for loop.
 */
 
@@ -41,22 +46,28 @@ let randomNumber = Math.floor(Math.random() * 3);
          return "scissors";  
       }
 } 
+
 // CREATE a function getHumanChoice
    // CREATE variable userInput
    // SET it as a GET input from user
    // INPUT from user should be case-insensitive
 // END
+
 function getHumanChoice() {
    let userInput = prompt("Enter rock, paper or scissors!");
    let formattedText = userInput.toLowerCase();
    return formattedText; 
-}
+}   
+
+
+// ! humanScore, ComputerScore and function playround was moved to playGame function ! 
+
 // CREATE humanScore and ComputerScore variables
 // SET them to 0 
 // END
 // let humanScore = 0;
 // let computerScore = 0;
-//  
+  
 // CREATE a function playRound that takes two parameters: humanChoice and computerChoice.
    // IF a humanChoice is equal to "rock" and computerChoice is equal to "rock" THEN
       // PRINT "It's a draw!"
@@ -69,61 +80,33 @@ function getHumanChoice() {
       // List the other six cases.
       //ENDIF
 //END
-/*
-function playRound(humanChoice, computerChoice) {
-   if(humanChoice === "rock" && computerChoice === "rock") {
-      console.log("It's draw!");
-   } else if(humanChoice === "rock" && computerChoice === "scissors") {
-      console.log("You win! Rock beats scissors.");
-      humanScore++;
-   } else if(humanChoice === "rock" && computerChoice === "paper") {
-      console.log("You lose! Paper beats rock.");
-      computerScore++;
-   } else if(humanChoice === "paper" && computerChoice === "paper") {
-      console.log("It's draw!");
-   } else if(humanChoice === "paper" && computerChoice === "rock") {
-      console.log("You win! Paper beats rock.");
-      humanScore++;
-   } else if(humanChoice === "paper" && computerChoice === "scissors") {
-      console.log("You lose! Scissors beats paper.");
-      computerScore++;
-   } else if(humanChoice === "scissors" && computerChoice === "scissors") {
-      console.log("It's draw!");
-   } else if(humanChoice === "scissors" && computerChoice === "paper") {
-      console.log("You win! Scissors beats paper.");
-      humanScore++;
-   } else {
-      console.log("You lose! Rock beats scissors.");
-      computerScore++;
-   } 
-}
-*/
+
+/* !Move to playGame function! 
+
 // CREATE variable humanSelection
 // SET it as a function getHumanChoice
 // END
 // CREATE variable computerSelection
 // SET it as a function getComputerChoice
 // END
-//let humanSelection;
-//let computerSelection;
+
 // DISPLAY human and computer selection
 //console.log(humanSelection);
 //console.log(computerSelection);
-// CALL a function playRound
- 
 
-// CREATE a function playGame 
-// MOVE playRound function to playGame function
+// CALL a function playRound
+ */
+
+// CREATE a function playGame (added roundsNumber as a parameter)  
 // MOVE humanScore and ComputerScore variables to playGame function
-// CREATE a loop that is iterated 5 times
+// MOVE playRound function to playGame function
+// ADD displaying the score after each round
+
+// CREATE a loop that is iterated 5 times (added number of iterations of the loop depending on roundsNumber)
    // CALL function playRound
    // DISPLAY string "Human Score: " and add humanScore "Computer Score: " and add computerScore
 // ENDLOOP
-// IF the value of the humanScore is greater than the computerScore
-   // DISPLAY string "You win! Your score is: " and add humanScore, "Computer Score is: " and add computerScore "Congratulations!"
-// ELSE
-   //DISPLAY string "You loose! Your score is: " and add humanScore, "Computer Score is: " and add computerScore.
-// ENDIF
+
 function playGame(roundsNumber) {
 
    let humanScore = 0;
@@ -158,25 +141,38 @@ function playGame(roundsNumber) {
       console.log("Human Score is: " + humanScore + " " + 
          "Computer Score is: " + computerScore);
    }
-   for (i = 0; i < roundsNumber; i++) {
-      let humanSelection = getHumanChoice();
-      let computerSelection = getComputerChoice();
-      console.log(humanSelection);
-      console.log(computerSelection);
-      playRound(humanSelection, computerSelection);
-   }
-   if (humanScore > computerScore) {
-      console.log("You win! Your score is: " + humanScore + " Computer Score is: " + computerScore + ". " + "Congratulations! Maybe you can try again?");
-   } else if (humanScore == computerScore) {
-      console.log("It's draw! Your score is: " + humanScore + " Computer Score is: " + computerScore + ". " + "Maybe you can try again?");
-   } else
-      console.log("You loose! Your score is: " + humanScore + " Computer Score is: " + computerScore + ". " + "Maybe you can try again?");
+      for (i = 0; i < roundsNumber; i++) {
+         
+         let humanSelection = getHumanChoice(); 
+         let computerSelection = getComputerChoice();
+         
+         console.log(humanSelection);
+         console.log(computerSelection);
+         playRound(humanSelection, computerSelection);
+      }
+   // DISPLAY return values of winnerDisplay function
+      console.log(winnerDisplay(humanScore, computerScore));
 }
 
-playGame(3);
+// CALL function playGame
+   playGame(3);
 
-// BONUS
+// CREATE a function winner display which takes 2 parameters: humanResult and computerResult 
+// IF the value of the humanResult is greater than the computerResult
+   // RETURN string "You win! Your score is: " and add humanResult, "Computer Score is: " and add computerResult then add "Congratulations! 
+   // Maybe you can try again?"
+// ELSE IF humanResult is equal to computerResult 
+   // RETURN string "It's a draw! Your score is: " then add humanResult then add " Computer Score is: " then add computerResult then add "Maybe you can try again?"
+// ELSE 
+   // RETURN string "You loose! Your score is: " then add humanResult then add " Computer Score is: " then add computerResult then add ". " then add "Maybe you can try again?"
+// ENDIF
+function winnerDisplay(humanResult, computerResult) {
+   if (humanResult > computerResult) {
+      return "You win! Your score is: " + humanResult + " Computer Score is: " + computerResult + ". " + "Congratulations! Maybe you can try again?";
+   } else if (humanResult == computerResult) {
+      return "It's a draw! Your score is: " + humanResult + " Computer Score is: " + computerResult + ". " + "Maybe you can try again?";
+   } else {
+      return "You loose! Your score is: " + humanResult + " Computer Score is: " + computerResult + ". " + "Maybe you can try again?";
+   }
+}
 
-// Add a check to see if the user has entered rock, paper or scissors, and if he has not, write out a message to that effect.
-// Dlaczego w petli for moge powtornie zadeklarowac te sama zmienna po kazdej iteracji?
-// Mozna dodac funkcje do wylonienia zwyciezcy  
