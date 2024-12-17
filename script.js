ab1b9d37326f932c78e873455e95eb08465ba34e
m0rv1nd <168345833+m0rv1nd@users.noreply.github.com>
2024-12-17 10:04:45 +0100

    project done

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const rounds = +prompt("How many rounds?");
+if (rounds == 0) {
+   window.location.reload();
+}
+
 const buttons = document.querySelectorAll("button");
+const divBtn = document.querySelector("#buttons");
 const humanPara = document.querySelector("#human");
 const computerPara = document.querySelector("#computer");
 const roundResult = document.querySelector("#round-result");
@@ -8,6 +13,7 @@ let humanScore = 0;
 let computerScore = 0;
 let humanSelection; 
 let computerSelection;
+let counter = 0;
 
 function getComputerChoice() { 
    let randomNumber = Math.floor(Math.random() * 3);
@@ -29,57 +35,58 @@ buttons.forEach((button) => {
       computerSelection = getComputerChoice();
       humanPara.textContent = "Human choice: " + humanSelection;
       computerPara.textContent = "Computer choice: " + computerSelection;
-      playGame(rounds)
+      counter++;
+      if (counter < rounds) {
+         playRound(humanSelection, computerSelection);
+      } else {
+         winnerDisplay(humanScore, computerScore);
+      }   
    });
 });
 
- function playGame() {
-   
-   function playRound(humanChoice, computerChoice) {
-      if(humanChoice === "rock" && computerChoice === "rock") {
-         roundResult.textContent = "It's draw!";
-      } else if(humanChoice === "rock" && computerChoice === "scissors") {
-         roundResult.textContent = "You win! Rock beats scissors.";
-         humanScore++;
-      } else if(humanChoice === "rock" && computerChoice === "paper") {
-         roundResult.textContent = "You lose! Paper beats rock.";
-         computerScore++;
-      } else if(humanChoice === "paper" && computerChoice === "paper") {
-         roundResult.textContent = "It's draw!";
-      } else if(humanChoice === "paper" && computerChoice === "rock") {
-         roundResult.textContent = "You win! Paper beats rock.";
-         humanScore++;
-      } else if(humanChoice === "paper" && computerChoice === "scissors") {
-         roundResult.textContent = "You lose! Scissors beats paper.";
-         computerScore++;
-      } else if(humanChoice === "scissors" && computerChoice === "scissors") {
-         roundResult.textContent = "It's draw!";
-      } else if(humanChoice === "scissors" && computerChoice === "paper") {
-         roundResult.textContent = "You win! Scissors beats paper.";
-         humanScore++;
-      } else {
-         roundResult.textContent = "You lose! Rock beats scissors.";
-         computerScore++;
-      }
-      roundScore.textContent = "Human Score is: " + humanScore + " " + 
-         "Computer Score is: " + computerScore; 
-   }
-      playRound(humanSelection, computerSelection);  
-} 
-
-/* function winnerDisplay(humanResult, computerResult) {
-   if (humanResult > computerResult) {
-      return "You win! Your score is: " + humanResult + " Computer Score is: " + computerResult + ". " + "Congratulations! Maybe you can try again?";
-   } else if (humanResult == computerResult) {
-      return "It's a draw! Your score is: " + humanResult + " Computer Score is: " + computerResult + ". " + "Maybe you can try again?";
+function playRound(humanChoice, computerChoice) {
+   if(humanChoice === "rock" && computerChoice === "rock") {
+      roundResult.textContent = "It's draw!";
+   } else if(humanChoice === "rock" && computerChoice === "scissors") {
+      roundResult.textContent = "You win! Rock beats scissors.";
+      humanScore++;
+   } else if(humanChoice === "rock" && computerChoice === "paper") {
+      roundResult.textContent = "You lose! Paper beats rock.";
+      computerScore++;
+   } else if(humanChoice === "paper" && computerChoice === "paper") {
+      roundResult.textContent = "It's draw!";
+   } else if(humanChoice === "paper" && computerChoice === "rock") {
+      roundResult.textContent = "You win! Paper beats rock.";
+      humanScore++;
+   } else if(humanChoice === "paper" && computerChoice === "scissors") {
+      roundResult.textContent = "You lose! Scissors beats paper.";
+      computerScore++;
+   } else if(humanChoice === "scissors" && computerChoice === "scissors") {
+      roundResult.textContent = "It's draw!";
+   } else if(humanChoice === "scissors" && computerChoice === "paper") {
+      roundResult.textContent = "You win! Scissors beats paper.";
+      humanScore++;
    } else {
-      return "You loose! Your score is: " + humanResult + " Computer Score is: " + computerResult + ". " + "Maybe you can try again?";
+      roundResult.textContent = "You lose! Rock beats scissors.";
+      computerScore++;
    }
-}  */
+   roundScore.textContent = "Human Score is: " + humanScore + " " + 
+      "Computer Score is: " + computerScore; 
+   }
 
-      /*   for (i = 0; i < roundsNumber; i++) {
-         if(i == roundsNumber) {
-            console.log("DONE");
-         }
-      } */
-
+function winnerDisplay(humanResult, computerResult) {
+   humanPara.remove();
+   computerPara.remove();
+   roundResult.remove();
+   divBtn.remove();
+   const winner = document.createElement("p");
+    
+   if (humanResult > computerResult) {
+      winner.textContent = "You win! Your score is: " + humanResult + " Computer Score is: " + computerResult + ". " + "Congratulations! Maybe you can try again?";
+   } else if (humanResult == computerResult) {
+      winner.textContent = "It's a draw! Your score is: " + humanResult + " Computer Score is: " + computerResult + ". " + "Maybe you can try again?";
+   } else {
+      winner.textContent = "You loose! Your score is: " + humanResult + " Computer Score is: " + computerResult + ". " + "Maybe you can try again?";
+   }
+   document.body.appendChild(winner);
+} 
